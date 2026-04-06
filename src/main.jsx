bd869310e744a827c74bfb40171b0eb3644cc4df
Joshua Mukisa <kiryowajoshua22@gmail.com>
2026-04-06 15:20:44 -0500

    Birthday Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import './index.css';
 import Home from './pages/Home/Home';
-import ValentinesForMonica from './pages/Valentines/Valentines';
-import RebuiltInGold from './pages/Sorry/Sorry';
 import FrenchLearner from './pages/French/French';
 import BirthdayForMonica from './pages/Birthday/Birthday';
 
